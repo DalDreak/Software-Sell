1061b93021c610dba9404d6327f078c40629abd4
Gabriel Gouveia <gabrielgouveia371@gmail.com>
2024-09-13 03:19:52 -0300

    v1.4 - Integração dos dados do back-end ao front-end da dashboard

--- dashboard.php
+++ dashboard.php
@@ -9,10 +9,10 @@ include_once("process/orders.php");
         <div class="row">
             <div class="col-md-12">
                 <h2>Gerenciar pedidos:</h2>
-                    </div>
-                        <div class="col-md-12 table-container">
-                        <table class="table">
-                        <thead>
+            </div>
+            <div class="col-md-12 table-container">
+                <table class="table">
+                    <thead>
                         <tr>
                             <th scope="col"><span>Pedido</span> #</th>
                             <th scope="col">Investimento</th>
@@ -23,34 +23,49 @@ include_once("process/orders.php");
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>    
-                            <td>#1</td>
-                            <td>Alto</td>
-                            <td>Gestão de vendas</td>
-                            <td>Manuntenção</td>
-                            <td>
-                                <form action="process/orders.php" method="POST" class="form-group update-form">
-                                    <input type="hidden" name="type" value="update">
-                                    <input type="hidden" name="id" value="1">
-                                    <select name="status" class="form-control status-input">
-                                        <option value="">Entrega</option>
-                                    </select>
-                                    <button type="submit" class="update-btn">
-                                        <i class="fas fa-sync-alt"></i>
-                                    </button>
-                                </form>
-                            </td>
-                            <td>
-                                <form action="process/orders.php" method="POST">
-                                    <input type="hidden" name="type" value="delete">
-                                    <input type="hidden" name="id" value="1">
-                                    <button type="submit" class="delete-btn">
-                                        <i class="fas fa-times"></i>
-                                    </button>
-                                </form>
-                            </td>
+                        <?php foreach ($softwares as $software): ?>
+                            <tr>
+                                <td><?= $software["id"] ?></td>
+                                <td><?= $software["investimento"] ?></td>
+                                <td><?= $software["tipo"] ?></td>
+                                <td>
+                                    <ul>
+                                        <?php foreach($software["pacotes_extra"] as $pacotes): ?>
+                                            <li>
+                                                <?= $pacotes; ?>
+                                            </li>
+                                        <?php endforeach; ?>
+                                    </ul>
+                                </td>
+                                <td>
+                                    <form action="process/orders.php" method="POST" class="form-group update-form">
+                                        <input type="hidden" name="type" value="update">
+                                        <input type="hidden" name="id" value="<?= $software["id"]?>">
+                                        <select name="status" class="form-control status-input">
+                                            <?php foreach($status as $s): ?>
+                                                <option value="<?= $s["id"] ?>" <?php echo($s["id"] == $software["status"]) ? "selected" : ""; ?>>
+                                                    <?= $s["tipo"]?>    
+                                                </option>
+                                            <?php endforeach; ?>    
+                                           
+                                        </select>
+                                        <button type="submit" class="update-btn">
+                                            <i class="fas fa-sync-alt"></i>
+                                        </button>
+                                    </form>
+                                </td>
+                                <td>
+                                    <form action="process/orders.php" method="POST">
+                                        <input type="hidden" name="type" value="delete">
+                                        <input type="hidden" name="id" value="<?= $software["id"]?>">
+                                        <button type="submit" class="delete-btn">
+                                            <i class="fas fa-times"></i>
+                                        </button>
+                                    </form>
+                                </td>
 
-                        </tr>
+                            </tr>
+                        <?php endforeach; ?>
                     </tbody>
                 </table>
             </div>
@@ -61,7 +76,3 @@ include_once("process/orders.php");
 <?php
 include_once("templates/footer.php");
 ?>
-
-
-
-    
